1a943c4fe72383f6a6ea7c3332514948add02dac
Branislav Holý <brano@holy.am>
2022-10-08 21:22:29 +0200

    fix: Typings for defined property

--- src/utils/js-utils.ts
+++ src/utils/js-utils.ts
@@ -1,4 +1,10 @@
-const definedProperty = <T>(obj: T, emptyValue?: unknown) => (Object.values(obj)[0] === emptyValue ? {} : obj);
+const definedProperty = <ObjectT extends Record<string, unknown>, EmptyValueT>(
+  obj: ObjectT,
+  emptyValue?: EmptyValueT
+) =>
+  Object.values(obj)[0] === emptyValue
+    ? ({} as Record<string, never>)
+    : (obj as Record<keyof ObjectT, Exclude<ObjectT[keyof ObjectT], EmptyValueT>>);
 
 export const JsUtils = {
   definedProperty
