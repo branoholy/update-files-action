17b0827e3a7793635ab479b5a3ebbc4991cf2045
Branislav Holý <brano@holy.am>
2023-10-08 00:13:09 +0200

    fix: Include deps in dist (#2393)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
+import { dependencies } from './package.json';
+
 export default defineConfig({
+  ssr: {
+    noExternal: Object.keys(dependencies)
+  },
   resolve: {
     alias: {
       ':': '/src'
